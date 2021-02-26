3618780ed8c56be8ddb94af8055c5154578d8419
LuisEliasTec <luiselias_1@hotmail.com>
2021-02-25 23:03:20 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd3e4d96d163fd22512a58db517c73157'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '230d5853-fe5d-4b64-8c2f-e43b78680936'
+};
